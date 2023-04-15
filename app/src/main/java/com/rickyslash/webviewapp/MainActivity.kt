6a76ead2e610d7da0d65022f88d8dff4a42021dd
ricky-kiva <ricky.kiva@gmail.com>
2023-04-15 21:18:31 +0700

    add WebView

--- app/src/main/java/com/rickyslash/webviewapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/webviewapp/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.rickyslash.webviewapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JsResult
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        // enable javascript on webView
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                // Preview alert by javascript alert
+                view?.loadUrl("javascript:alert('Web is successfully loaded')")
+            }
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            // override if there is Javascript alert
+            override fun onJsAlert(
+                view: WebView?,
+                url: String?,
+                message: String?,
+                result: JsResult?
+            ): Boolean {
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+                // this ensure that user has confirmed the Javascript (like when they click 'ok' on JS alert)
+                result?.confirm()
+                return true
+            }
+        }
+
+        // load Url on webView
+        webView.loadUrl("https://www.dicoding.com")
     }
 }
 
