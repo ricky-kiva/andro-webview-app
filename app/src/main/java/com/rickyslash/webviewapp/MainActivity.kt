56de3239762ea51fd2096a08eaded5a88274baf7
ricky-kiva <ricky.kiva@gmail.com>
2023-04-15 20:48:08 +0700

    add summary

--- app/src/main/java/com/rickyslash/webviewapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/webviewapp/MainActivity.kt
@@ -9,3 +9,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+// WebView used to display web page as part of Activity
+// Additional adjustment on WebView such:
+// - fullscreen with WebChromeClient
+// --- WebChromeClient also called when WebView need to change UI, like closing/opening window, or send Javascript dialog
+// - handle event that affect rendering process (error, navigation with WebViewClient)
+// - activate Javascript by modifying WebSettings
+// - use javascript to access Android Framework object
